7102fc5fbdd8e9551e5289a5598cd66421b0de50
LG <LG@DESKTOP-L1958T5>
2024-02-10 22:10:45 +0530

    added slide

---
+++ SampleApiProject/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleApiProject.Models;
+
+public partial class Student
+{
+    public int Sid { get; set; }
+
+    public string? Sname { get; set; }
+
+    public decimal? Fees { get; set; }
+}
